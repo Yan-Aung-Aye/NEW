f6d22fc48ad64f0074cea36d972e52ac06b09530
Yan <yanaung3424@gmail.com>
2024-08-17 11:01:24 +0630

    ayemithu

--- Learning/yan.js
+++ Learning/yan.js
@@ -1,5 +1,15 @@
 
-let num1 = 10;
-let num2 = 20;
-const num3 = num1 + num2
-console.log(num3)
+// let num1 = 10;
+// let num2 = 20;
+// const num3 = num1 + num2
+// console.log(num3)
+
+let person = {
+    name: 'yan aung',
+    age: 25,
+    hobby: 'footballsoccer'
+
+}
+
+console.log(person)
+console.log(person.age)
