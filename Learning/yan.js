f8a230af8922a8802a326a970bf1b27030e4fdab
Yan <yanaung3424@gmail.com>
2024-08-17 20:14:56 +0630

    git conflict

--- Learning/yan.js
+++ Learning/yan.js
@@ -1,15 +1,2 @@
-
-// let num1 = 10;
-// let num2 = 20;
-// const num3 = num1 + num2
-// console.log(num3)
-
-let person = {
-    name: 'yan aung',
-    age: 25,
-    hobby: 'footballsoccer'
-
-}
-
-console.log(person)
-console.log(person.age)
+let num = [1, 2, 4, 5, 3, 6, 1009]
+console.log(num);
